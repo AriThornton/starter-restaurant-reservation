20e7314440108df7eee9277de91e71b956ec3987
Ari Thornton <73207741+AriThornton@users.noreply.github.com>
2021-04-30 18:30:03 -0400

    Update tables.router.js

--- back-end/src/tables/tables.router.js
+++ back-end/src/tables/tables.router.js
@@ -1,19 +1,22 @@
-const router = require("express").Router();
-const controller = require("./tables.controller");
-const methodNotAllowed = require("../errors/methodNotAllowed");
+/**
+ * Defines the router for tables resources.
+ *
+ * @type {Router}
+ */
+
+const router = require('express').Router()
+const controller = require('./tables.controller')
+const methodNotAllowed = require('../errors/methodNotAllowed')
+const seatRouter = require('../seat/seat.router')
+
+router.use('/:table_id/seat', controller.tableExists, seatRouter)
+
+router.route('/:table_id').get(controller.read).all(methodNotAllowed)
 
 router
-  .route("/")
+  .route('/')
   .get(controller.list)
   .post(controller.create)
-  .all(methodNotAllowed);
+  .all(methodNotAllowed)
 
-router
-  .route("/:table_id/seat")
-  .put(controller.update)
-  .delete(controller.delete)
-  .all(methodNotAllowed);
-
-router.route("/:table_id").get(controller.read).all(methodNotAllowed);
-
-module.exports = router;
+module.exports = router
